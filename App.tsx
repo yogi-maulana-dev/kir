bd751d778540ece506d96b2c5d4c528f8bd12d18
yogi-maulana-dev <yogi.maulana.2021211032@mail.darmajaya.ac.id>
2024-11-14 05:53:26 +0700

    proses 1

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import {Text, View} from 'react-native';
 import React, {Component} from 'react';
+import {Text, Button, TextInput, View, StyleSheet} from 'react-native';
 
-export default class App extends Component {
+export class App extends Component {
   render() {
     return (
-      <View>
-        <Text>App</Text>
+      <View style={styles.container}>
+        <TextInput placeholder="Enter text here" style={styles.input} />
+        <Button title="TULISAN" onPress={() => alert('Button pressed')} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    width: '80%',
+  },
+});
+
+export default App;

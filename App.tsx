84449c3c98ca3afcdc708fa0aa3e1d27c301d276
yogi-maulana-dev <yogi.maulana.2021211032@mail.darmajaya.ac.id>
2024-11-14 06:21:48 +0700

    proses 2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
-import {Text, Button, TextInput, View, StyleSheet} from 'react-native';
+import {
+  Text,
+  Button,
+  TextInput,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+function colek() {
+  alert('Meow...');
+}
 
 export class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text style={{fontSize: 50, fontWeight: 'bold'}}>Selamat Datang</Text>
+
+        <Image source={require('./gambar/images.jpeg')} style={styles.image} />
+
+        {/* Uncomment this if you'd like to use an image from a URL instead */}
+        {/* 
+        <Image
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrGLb1IXdjommJUROhJZl5nF0VWAohIPNFBrpQ8Iv-9BahNHqy30B9INrehNM7A-k68Es',
+          }}
+          style={styles.image}
+        /> 
+        */}
+
         <TextInput placeholder="Enter text here" style={styles.input} />
-        <Button title="TULISAN" onPress={() => alert('Button pressed')} />
+
+        <Button title="TULISAN" onPress={colek} />
+
+        <TouchableOpacity onPress={colek} style={styles.touchable}>
+          <Text style={styles.touchableText}>Colek Aku Jugah</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -18,6 +49,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  image: {
+    width: 300,
+    height: 300,
+    marginVertical: 20, // Add vertical margin to create space above and below the image
+  },
   input: {
     height: 40,
     borderColor: 'gray',
@@ -25,6 +61,18 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 8,
     width: '80%',
+  },
+  touchable: {
+    backgroundColor: 'red',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  touchableText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
